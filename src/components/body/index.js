6e959dfc00451aa89ca2a911e88f8696a88c89f5
Adityaraj-30 <imadityaraj00@gmail.com>
2022-01-16 00:43:58 +0530

    Added components

---
+++ src/components/body/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Body() {
+    return (
+        <div>
+            This is a Body
+        </div>
+    )
+}
+
+export default Body
---
+++ src/components/header/index.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import "./header.css";
+import Mobile from "./mobile/index";
+import Web from "./web/index";
+import { FaListUl } from "react-icons/fa";
+function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleLogoClick = () => {
+    window.scrollTo(0, 0);
+  };
+  window.onscroll = function () {
+    setIsOpen(false);
+  };
+  return (
+    <div className="header">
+      <div onClick={handleLogoClick} className="logo">
+        Aditya Raj
+      </div>
+      <div className="menu">
+        <div className="web-menu">
+          <Web />
+        </div>
+
+        <div className="mobile-menu">
+          <div onClick={() => setIsOpen(!isOpen)}>
+            <div className="menu-icon">
+              <FaListUl />
+            </div>
+          </div>
+          {isOpen && <Mobile isOpen={isOpen} setIsOpen={setIsOpen} />}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
